a44739216971501124ecb29f7186fdb70b9a8472
MoniTHOR--Project <46280154+ebenhamu@users.noreply.github.com>
2024-12-19 22:30:09 +0200

    Update swarm.sh

--- swarm.sh
+++ swarm.sh
@@ -1,8 +1,15 @@
 docker swarm init   
 
+
 # docker swarm join --token <token> <manager-node-ip>:2377
 
+
 docker node ls  / list nodes 
+
+docker node promote worker1
+
+# make a node to be a manager 
+docker node promote <node name >   
 
 docker service ps
 
